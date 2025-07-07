d10b90006060a0dbf30a1192bc2f273a209815a9
Chhaya <tyagi.chhaya4@gmail.com>
2025-07-07 13:30:45 +0530

    Longest Subarray Better Approach

---
+++ src/SlidingWindow/LongestSubarrayII.java
@@ -0,0 +1,30 @@
+package SlidingWindow;
+
+//Better Approach-->O(2N)
+
+public class LongestSubarrayII {
+    public static void main(String[] args) {
+        int[] arr={2,5,20,10,10};
+        int k=20;
+        int ans=maxLengthSubarray(arr,k);
+        System.out.println(ans);
+    }
+    //Better Approach ---> By Shrinking and Expanding the  window
+    public static int maxLengthSubarray(int[] arr,int k){
+        int maxLen=0,sum=0;
+        int left=0,right=0;
+        while(right<arr.length-1){
+            sum+=sum+arr[right];
+
+            //Shrinking the window when condition is invalid
+            while(sum>k){
+                sum-=arr[left];
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+        return maxLen;
+    }
+
+}
